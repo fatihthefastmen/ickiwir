0a24e7dbe805bf8e4e284e0515e6ca5a22d1c633
fatihthefastmen <122343971+fatihthefastmen@users.noreply.github.com>
2023-01-10 14:16:59 +0700

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,11 @@
+Bilangan1=int(input("Angka1="))
+Bilangan2=int(input("Angka 2="))
+Tambah=Bilangan1+Bilangan2
+print(Bilangan1+Bilangan2)
+print(f"{Bilangan1}+{Bilangan2}={Tambah}")
+
+if Tambah%2==0 :
+       print ("Ini genap")
+else :
+    print("Ini ganjil")
+
